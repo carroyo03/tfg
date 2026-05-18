6a6c2c1986e6d1e7f970bc1614c81ac726ce8840
Carlos Arroyo <carroyo1@kpmg.es>
2026-05-18 11:54:32 +0200

    fix: correct import path in chakra_color_mode_provider.js and update dependencies in pyproject.toml and uv.lock

--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,12 @@
 import reflex as rx
 from dotenv import load_dotenv
+from reflex_components_radix import RadixThemesPlugin
 import os
 load_dotenv()
 
 config = rx.Config(
     app_name=os.getenv("REFLEX_APP_NAME", "tfg_app"),
     show_built_with_reflex=False,
-    disable_plugins=["SitemapPlugin"]
+    disable_plugins=["SitemapPlugin"],
+    plugins=[RadixThemesPlugin()]
 )
