9b056ff658548c93b23825f327c20d19a7720120
Carlos Arroyo <carroyo1@kpmg.es>
2026-05-18 11:59:16 +0200

    fix: update disable_plugins to use SitemapPlugin reference in rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,12 +1,13 @@
 import reflex as rx
 from dotenv import load_dotenv
 from reflex_components_radix.plugin import RadixThemesPlugin
+from reflex_base.plugins.sitemap import SitemapPlugin
 import os
 load_dotenv()
 
 config = rx.Config(
     app_name=os.getenv("REFLEX_APP_NAME", "tfg_app"),
     show_built_with_reflex=False,
-    disable_plugins=["SitemapPlugin"],
+    disable_plugins=[SitemapPlugin],
     plugins=[RadixThemesPlugin()]
 )
